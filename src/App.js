d8974ac0424135e12250319eba7097760819eede
cerengunaydin <crnn.gnydnn@gmail.com>
2024-09-03 23:19:36 +0300

    login-screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css'
+import LoginForm from "./components/loginform/LoginForm"
+
+function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
